992197d43d34134577b66f7b3049f68e316fce04
Shahriyar Sheikh <m.shahriyarsheikh@gmail.com>
2019-07-25 16:28:47 +0500

    added escrow function in escrow contract

--- contracts/BountryEscrowContract.sol
+++ contracts/BountryEscrowContract.sol
@@ -3,4 +3,42 @@ pragma experimental ABIEncoderV2;
 
 contract BountyEscrowContract {
     
+    event EscrowStatus(string status);
+    
+    address _bountyOwner;
+    address _bountyAvailedBy;
+    bytes32 _bountyHash;
+    uint _totalSecurityDeposit;
+    address _owner;
+    uint _arbitratorFees;
+    uint _amountLocked;
+    
+    constructor(address bountyOwner, address bountyAvailedBy, bytes32 bountyHash, uint totalSecurityDeposit, address arbitrator, uint arbitratorFees) public payable {
+        _bountyOwner = bountyOwner;
+        _bountyAvailedBy = bountyAvailedBy;
+        _bountyHash = bountyHash;
+        _totalSecurityDeposit = totalSecurityDeposit;
+        _owner = arbitrator;
+        _arbitratorFees = arbitratorFees;
+        _amountLocked = msg.value;
+    }
+    
+    
+    function completeEscrow() external payable{
+        
+    }
+    
+    //sends all security deposit to the users
+    function resetEscrow() external payable{
+        
+    }
+    
+    //sends all security deposit to owner of main contract on failure of proposed task
+    function failEscrow() external payable {
+        
+    }
+    
+    
+    
+    
 }
--- contracts/BountyMainContract.sol
+++ contracts/BountyMainContract.sol
@@ -1,10 +1,16 @@
 pragma solidity >=0.4.22 <0.6.0;
 pragma experimental ABIEncoderV2;
 
+import "./BountyEscrowContract.sol";
+
 
 contract BountyMainContract {
     
+    event BountyPlaced(address bountyOwner, bytes32 bountyHash);
+    
     address payable _owner;
+    uint _fixedSecurityDeposit = 0.1 ether;
+    BountyEscrowContract _bountyEscrowContract;
     
     modifier onlyOwner {
         require(msg.sender == _owner);
@@ -25,6 +31,7 @@ contract BountyMainContract {
     mapping (address => mapping(bytes32=> Bounty)) _bountiesByOwnerAddress;
     mapping (address => BountyOwners) _bountyOwnerDetails;
     mapping (bytes32 => Bounty) _bountiesDetailsById;
+    mapping (bytes32 => address) public escrowAddressWithRespectToBountyHash;
     
     struct BountyOwners {
         string name;
@@ -39,6 +46,7 @@ contract BountyMainContract {
         string detailsUrl;
         bool isAvailed;
         address isAvailedBy;
+        uint price;
     }
     
     constructor() public {
@@ -59,16 +67,16 @@ contract BountyMainContract {
     }
     
     
-    function addBounty(string calldata _name, string calldata _detailsUrl) external isBountyOwner returns (bytes32){
-        bytes32 _bountyId = keccak256(abi.encodePacked(_name,_detailsUrl));
+    function addBounty(string calldata _name, string calldata _detailsUrl) external payable isBountyOwner returns (bytes32){
         
+        require(msg.value == _fixedSecurityDeposit,"Security Deposit must be made first");
+        
+        bytes32 _bountyId = keccak256(abi.encodePacked(_name,_detailsUrl));
       
         Bounty storage currentBounties = _bountiesByOwnerAddress[msg.sender][_bountyId];
         
         currentBounties.name = _name;
         currentBounties.detailsUrl = _detailsUrl;
-        
-        //
         
         _bountiesDetailsById[_bountyId] = currentBounties;
         
@@ -92,7 +100,20 @@ contract BountyMainContract {
     }
     
     function availBounty(bytes32 _bountyId) external payable returns(bool){
+       Bounty storage _bountyToAvail = _bountiesDetailsById[_bountyId];
+       require(_bountyToAvail.isAvailed == false,"Bounty already availed or doesn't exist");
+       require(_fixedSecurityDeposit == msg.value);
+       
+       uint _arbitratorFee = 0.01 ether;
+       
+       _bountyEscrowContract = (new BountyEscrowContract).value(msg.value)(address(0),msg.sender,_bountyId,_fixedSecurityDeposit * 2,address(0),_arbitratorFee);
+       escrowAddressWithRespectToBountyHash[_bountyId] = msg.sender;
+       
         //To be continued
+    }
+    
+    function getBountyValue() external view returns(uint){
+        return address(this).balance;
     }
     
 }
